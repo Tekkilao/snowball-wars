3daa714d060b643ee0029c6fb047ce2c7b1a46ee
Tekkila <gordogamer8@gmail.com>
2023-11-23 09:31:27 -0300

    Map added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require("socket.io");
+const loadMap = require('./mapLoader');
 
 const app = express();
-
+const httpServer = createServer(app);
 const port = 5000;
 
+async function main() { 
 
-app.listen(port, () => {
-    date = new Date()
-    console.log(`Server Started on port ${port} at ${date}`);
-});
+    const map2D = await loadMap();
+    
+    const io = new Server(httpServer, {
+        cors: {
+            origin:"http://localhost:5173"
+        }
+    });
+    
+    io.on("connect", (socket) => {
+        console.log('user connected', socket.id);
+        socket.emit('map', map2D);
+    });
+    
+    httpServer.listen(port, () => {
+        date = new Date()
+        console.log(`Server Started on port ${port} at ${date}`);
+    });
+}
+
+main();
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,62 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+  import { io } from "socket.io-client";
+  import { onMounted } from "vue";
+  const mapImage = new Image();
+  mapImage.src = 'src/assets/snowy-sheet.png';
+
+    onMounted( () => {
+      const canvasEl = document.getElementById('canvas');
+      canvasEl.width = window.innerWidth;
+      canvasEl.height = window.innerHeight;
+      const canvas = canvasEl.getContext("2d");
+      const socket = io("ws://localhost:5000");
+
+      let map = [[]];
+      const TILE_SIZE = 16;
+
+      socket.on('connect', () => {
+      });
+
+      socket.on('map', (loadedMap) => {
+        map = loadedMap;
+      });
+
+      function loop() {
+        canvas.clearRect(0, 0, canvas.width, canvas.height);
+        const TILES_IN_ROW = 8;
+
+        for (let row = 0; row < map.length; row++){
+          for (let col = 0; col < map[0].length; col++){
+            const { id } = map[row][col];
+            const imageRow = parseInt(id / TILES_IN_ROW);
+            const imageCol = id % TILES_IN_ROW;
+            canvas.drawImage(mapImage,
+            imageCol * TILE_SIZE,
+            imageRow * TILE_SIZE,
+            TILE_SIZE,
+            TILE_SIZE,
+            col * TILE_SIZE,
+            row * TILE_SIZE,
+            TILE_SIZE,
+            TILE_SIZE
+            );
+          }
+        }
+
+        canvas.fillStyle = '#ff0000';
+        canvas.fillRect(0, 0, 10, 10);
+        window.requestAnimationFrame(loop);
+      }
+      window.requestAnimationFrame(loop);
+    });
+
+
+
+
+</script> 
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <canvas id="canvas"></canvas>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
