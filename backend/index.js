4a5ff2f6469ac32b7d75ecdf477aae1ba095a789
Tekkila <gordogamer8@gmail.com>
2023-11-30 18:47:22 -0300

    0.3 version

--- backend/index.js
+++ backend/index.js
@@ -7,25 +7,124 @@ const app = express();
 const httpServer = createServer(app);
 const port = 5000;
 
-async function main() { 
+const io = new Server(httpServer, {
+    cors: {
+        origin:"http://localhost:5173"
+    }
+});
 
-    const map2D = await loadMap();
-    
-    const io = new Server(httpServer, {
-        cors: {
-            origin:"http://localhost:5173"
+const TICK_RATE = 64;
+
+const SPEED = 5;
+const SNOWBALL_SPEED = 7;
+
+let players = [];
+let snowballs = [];
+const inputsMap = {};
+
+
+function tick(delta) {
+    for(const player of players) {
+        const inputs = inputsMap[player.id];
+        // switch (inputs) {
+        //     // case inputs.up: 
+        //     //     player.y -= SPEED;
+        //     // case inputs.down:
+        //     //     player.y += SPEED;
+        //     // case inputs.left:
+        //     //     player.x -= SPEED;
+        //     case inputs.right == true: 
+        //         player.x = 0
+        //         console.log('lixo')
+        //         break;
+
+        // }
+        if (inputs.up) {
+            player.y -= SPEED;
+        } else if (inputs.down) {
+            player.y += SPEED;
+        } else if (inputs.left) {
+            player.x -= SPEED;
+        } else if (inputs.right) {
+            player.x += SPEED;
         }
-    });
+    }
+    for (const snowball of snowballs) {
+        snowball.x += Math.cos(snowball.angle) * SNOWBALL_SPEED;
+        snowball.y += Math.sin(snowball.angle) * SNOWBALL_SPEED;
+        snowball.timeLeft -= delta;
+    }
     
+    snowballs = snowballs.filter((snowball) => snowball.timeLeft > 0);
+
+    io.emit('players', players);    
+    io.emit("snowballs", snowballs);
+
+};
+
+// for (const snowball of snowballs) {
+//     snowball.x += Math.cos(snowball.angle) * SNOWBALL_SPEED;
+//     snowball.y += Math.sin(snowball.angle) * SNOWBALL_SPEED;
+// }
+
+
+async function main() { 
+    const map2D = await loadMap();
+
     io.on("connect", (socket) => {
         console.log('user connected', socket.id);
+
+        inputsMap[socket.id] = {
+            up: false,
+            down: false,
+            right: false,
+            left: false
+        };
+
+        players.push({
+            id: socket.id,
+            x: 800, 
+            y: 800,
+        });        
+
         socket.emit('map', map2D);
+
+        socket.on('inputs', (inputs) => {
+            inputsMap[socket.id] = inputs;
+        });
+        
+        socket.on('snowball', (angle) => {
+            const player = players.find(player => player.id === socket.id);
+            snowballs.push({
+                angle,
+                x: player.x,
+                y: player.y,
+                timeLeft: 4000, 
+            })
+        })
+
+        socket.on('disconnect', () => {
+            players = players.filter((player) => player.id !== socket.id);
+            // setInterval(() => {
+            //     players = players.filter((player) => player.id !== socket.id);
+            // }, 60000);
+        });
     });
     
     httpServer.listen(port, () => {
         date = new Date()
         console.log(`Server Started on port ${port} at ${date}`);
     });
+
+    let lastUpdate = Date.now();
+
+    setInterval(() => {
+        const now = Date.now();
+        const delta = now - lastUpdate;
+        tick(delta);
+        lastUpdate = now;
+    }, 1000 / TICK_RATE);
+    
 }
 
 main();
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
   import { io } from "socket.io-client";
   import { onMounted } from "vue";
-  const mapImage = new Image();
-  mapImage.src = 'src/assets/snowy-sheet.png';
+;
+
 
     onMounted( () => {
+      const mapImage = new Image();
+      mapImage.src = 'src/assets/snowy-sheet.png';
+      const santaImage = new Image();
+      santaImage.src = 'src/assets/santa.png'
+
       const canvasEl = document.getElementById('canvas');
       canvasEl.width = window.innerWidth;
       canvasEl.height = window.innerHeight;
@@ -12,7 +17,11 @@
       const socket = io("ws://localhost:5000");
 
       let map = [[]];
-      const TILE_SIZE = 16;
+      let players = [];
+      let snowballs = [];
+      
+
+      const TILE_SIZE = 32;
 
       socket.on('connect', () => {
       });
@@ -20,31 +29,114 @@
       socket.on('map', (loadedMap) => {
         map = loadedMap;
       });
+      
+      socket.on('players', (serverPlayers) => {
+        players = serverPlayers;
+      })
+
+      socket.on("snowballs", (serverSnowballs) => {
+        snowballs = serverSnowballs;
+      });
+
+
+      const inputs = {
+        up: false,
+        down: false,
+        right: false,
+        left: false
+      }
+
+      window.addEventListener('keydown', (e) => {
+        const key = e.key.toLowerCase();
+        
+        switch(key) {
+          case "w": 
+            inputs['up'] = true;
+            break;
+          case "s": 
+            inputs['down'] = true;
+            break;
+          case "d":
+            inputs['right'] = true;
+            break;
+          case "a": 
+            inputs['left'] = true;
+            break;
+        }
+        socket.emit('inputs', inputs);
+      
+
+      })
+      window.addEventListener('keyup', (e) => {
+        const key = e.key.toLowerCase();
+        switch(key) {
+          case "w": 
+            inputs['up'] = false;
+            break;
+          case "s": 
+            inputs['down'] = false;
+            break;
+          case "d":
+            inputs['right'] = false;
+            break;
+          case "a": 
+            inputs['left'] = false;
+            break;
+        }
+        socket.emit('inputs', inputs);
+
+      })
+      window.addEventListener('click', (e) => {
+        const player = players.find((player) => player.id === socket.id);
+        const mouseX = e.clientX - canvasEl.getBoundingClientRect().left;
+        const mouseY = e.clientY - canvasEl.getBoundingClientRect().top;
+        const angle = Math.atan2(mouseY - player.y, mouseX - player.x);
+        socket.emit("snowball", angle);
+      })
+
 
       function loop() {
-        canvas.clearRect(0, 0, canvas.width, canvas.height);
+        canvas.clearRect(0, 0, canvasEl.width, canvasEl.height);
         const TILES_IN_ROW = 8;
+        const myPlayer = players.find((player) => player.id === socket.id);
+        let cameraX = 0;
+        let cameraY = 0;
+        if (myPlayer) {
+          cameraX = parseInt(myPlayer.x - canvasEl.width / 2);
+          cameraY = parseInt(myPlayer.y - canvasEl.height / 2);
+        }
+
 
         for (let row = 0; row < map.length; row++){
           for (let col = 0; col < map[0].length; col++){
             const { id } = map[row][col];
             const imageRow = parseInt(id / TILES_IN_ROW);
             const imageCol = id % TILES_IN_ROW;
-            canvas.drawImage(mapImage,
+            canvas.drawImage(
+            mapImage,
             imageCol * TILE_SIZE,
             imageRow * TILE_SIZE,
             TILE_SIZE,
             TILE_SIZE,
-            col * TILE_SIZE,
-            row * TILE_SIZE,
+            col * TILE_SIZE - cameraX,
+            row * TILE_SIZE - cameraY,
             TILE_SIZE,
             TILE_SIZE
             );
           }
         }
 
-        canvas.fillStyle = '#ff0000';
-        canvas.fillRect(0, 0, 10, 10);
+        for (const player of players) {
+        canvas.drawImage(santaImage, player.x - cameraX, player.y - cameraY);
+        };
+        
+        for (const snowball of snowballs) {
+          canvas.fillStyle = "#FFFFFF"
+          canvas.beginPath();
+          canvas.arc(snowball.x - cameraX, snowball.y - cameraY, 3, 0, 2 * Math.PI);
+          canvas.fill()
+        }
+        
         window.requestAnimationFrame(loop);
       }
       window.requestAnimationFrame(loop);
